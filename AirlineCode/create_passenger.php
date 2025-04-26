c1ecf6f823aeb366cdd9ee0e08d1cfad6d2e8138
christian dees <christianmdees@mgail.com>
2025-04-26 17:58:02 -0600

    put in folder, just copy folder when transferring to db

--- AirlineCode/create_passenger.php
+++ AirlineCode/create_passenger.php
@@ -65,10 +65,8 @@ if (isset($_POST['Submit'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>AeroMate - Create Account</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Font Awesome for icons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link rel="stylesheet" href="styles.css">
-
 </head>
 <body>
   <div class="account-create-container">
--- AirlineCode/index.php
+++ AirlineCode/index.php
@@ -46,7 +46,6 @@ if (!empty($_POST) && isset($_POST['Submit'])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>AeroMate Login</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Font Awesome for icons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <style>
     body {
